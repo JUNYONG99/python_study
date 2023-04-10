fbb118bf6fdf06a6022a7f210e9f23ad30cf4a80
junyong <aapp76sun@gmail.com>
2023-04-10 14:49:11 +0900

    update python libray randon

---
+++ study/python_standard_libray_random.py
@@ -0,0 +1,12 @@
+from random import randint
+
+pc_choice = randint(1, 50)  # import random module
+while True:
+  user_choice = int(input("Choose number : "))
+  if user_choice == pc_choice:
+    print("You won! Comuputer Choose", pc_choice)
+    break
+  elif user_choice > pc_choice:
+    print("Lower!")
+  elif user_choice < pc_choice:
+    print("Higher!")
