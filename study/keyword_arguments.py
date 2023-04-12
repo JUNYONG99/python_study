a30e4ce48bd1b65c648b3d3ea6e9a7328cbaacd9
junyong <aapp76sun@gmail.com>
2023-04-12 15:28:14 +0900

    edit keyword arguments.py

---
+++ study/keyword_arguments.py
@@ -0,0 +1,9 @@
+# Keyword Arguments
+def say_hello(name, age):
+  print(f"Hello {name} you are {age} years old")
+
+
+# positional
+say_hello("nico", 12)
+# If you write your name in Argument, positional doesn't matter
+say_hello(age=12, name="nico")
